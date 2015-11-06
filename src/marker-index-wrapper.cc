1ec9f67683dc1bbc7fd881cf2b1e45f6db6bf743
Nathan Sobo <nathan@github.com>
2015-11-05 21:24:06 -0700

    Expose insert function to JS that takes an id and 4 numbers

--- src/marker-index-wrapper.cc
+++ src/marker-index-wrapper.cc
@@ -13,6 +13,7 @@ public:
         Nan::New<String>("MarkerIndex").ToLocalChecked());
     constructorTemplate->InstanceTemplate()->SetInternalFieldCount(1);
     constructorTemplate->PrototypeTemplate()->Set(Nan::New<String>("generateRandomNumber").ToLocalChecked(), Nan::New<FunctionTemplate>(GenerateRandomNumber)->GetFunction());
+    constructorTemplate->PrototypeTemplate()->Set(Nan::New<String>("insert").ToLocalChecked(), Nan::New<FunctionTemplate>(Insert)->GetFunction());
     module->Set(Nan::New("exports").ToLocalChecked(),
                 constructorTemplate->GetFunction());
   }
@@ -27,6 +28,22 @@ private:
     MarkerIndexWrapper *wrapper = Nan::ObjectWrap::Unwrap<MarkerIndexWrapper>(info.This());
     int random = wrapper->marker_index.GenerateRandomNumber();
     info.GetReturnValue().Set(Nan::New<v8::Number>(random));
+  }
+
+  static NAN_METHOD(Insert) {
+    MarkerIndexWrapper *wrapper = Nan::ObjectWrap::Unwrap<MarkerIndexWrapper>(info.This());
+
+    String::Utf8Value id_utf_8_value {info[0]};
+    string id {*id_utf_8_value};
+
+    Local<Number> start_row = Local<Number>::Cast(info[1]);
+    Local<Number> start_column = Local<Number>::Cast(info[2]);
+    Local<Number> end_row = Local<Number>::Cast(info[3]);
+    Local<Number> end_column = Local<Number>::Cast(info[4]);
+    Point start_point {static_cast<unsigned>(start_row->Int32Value()), static_cast<unsigned>(start_column->Int32Value())};
+    Point end_point {static_cast<unsigned>(end_row->Int32Value()), static_cast<unsigned>(end_column->Int32Value())};
+
+    wrapper->marker_index.Insert(id, start_point, end_point);
   }
 
   MarkerIndexWrapper(v8::Local<v8::Number> seed) :
