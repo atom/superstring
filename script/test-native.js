ba2902d2772f36032b887825a556be60b7c79b82
Nathan Sobo <nathan@github.com>
2016-12-20 19:46:19 -0700

    Always node-gyp build in test-native script

--- script/test-native.js
+++ script/test-native.js
@@ -6,7 +6,9 @@ const childProcess = require('child_process')
 
 const testsPath = path.resolve(__dirname, '..', 'build', 'Debug', 'tests')
 
-if (!fs.existsSync(testsPath)) {
+if (fs.existsSync(testsPath)) {
+  childProcess.spawnSync('node-gyp', ['build'], {stdio: 'inherit'})
+} else {
   console.log('Building tests binary in debug configuration...')
   childProcess.spawnSync('node-gyp', ['rebuild', '--debug', '--tests'], {stdio: 'inherit'})
 }
