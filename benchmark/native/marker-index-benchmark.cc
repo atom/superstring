1912645f273365d2a759fc9e87b193a5a8c3c93d
Nathan Sobo <nathan@github.com>
2017-01-15 10:42:47 -0700

    Convert tests and benchmarks to new naming convention

--- benchmark/native/marker-index-benchmark.cc
+++ benchmark/native/marker-index-benchmark.cc
@@ -10,7 +10,7 @@
 using namespace std::chrono;
 using std::vector;
 
-Range GetRandomRange() {
+Range get_random_range() {
   Point start(rand() % 100, rand() % 100);
   Point end = start;
   if (rand() % 10 < 5) {
@@ -20,19 +20,19 @@ Range GetRandomRange() {
 }
 
 
-TEST_CASE("MarkerIndex::Insert") {
+TEST_CASE("MarkerIndex::insert") {
   srand(0);
   MarkerIndex marker_index;
   vector<Range> ranges;
   uint count = 20000;
 
   for (uint i = 0; i < count; i++) {
-    ranges.push_back(GetRandomRange());
+    ranges.push_back(get_random_range());
   }
 
   milliseconds start = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
   for (uint i = 0; i < count; i++) {
-    marker_index.Insert(i, ranges[i].start, ranges[i].end);
+    marker_index.insert(i, ranges[i].start, ranges[i].end);
   }
   milliseconds end = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
   std::cout << "Inserting " << (end - start).count();
