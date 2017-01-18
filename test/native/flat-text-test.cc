d2dd4edb795b9b02805819f3dc7a92590ad68df5
Nathan Sobo <nathan@github.com>
2017-01-18 14:34:42 -0700

    Add a test for partial characters at the end of the stream

--- test/native/flat-text-test.cc
+++ test/native/flat-text-test.cc
@@ -18,7 +18,7 @@ TEST_CASE("can build a FlatText from a UTF8 stream") {
   REQUIRE(progress_reports == vector<size_t>({3, 5, 8, 11, 14, 17, 18}));
 }
 
-TEST_CASE("replaces invalid byte sequences with the Unicode replacement character") {
+TEST_CASE("replaces invalid byte sequences in the middle of the stream with the Unicode replacement character") {
   string input = "ab" "\xc0" "\xc1" "de";
   stringstream stream(input, std::ios_base::in);
 
@@ -29,6 +29,15 @@ TEST_CASE("replaces invalid byte sequences with the Unicode replacement characte
 
   REQUIRE(text == FlatText { u"ab" "\ufffd" "\ufffd" "de" });
   REQUIRE(progress_reports == vector<size_t>({ 3, 6 }));
+
+}
+
+TEST_CASE("replaces invalid byte sequences at the end of the stream with the Unicode replacement characters") {
+  string input = "ab" "\xf0\x9f"; // incomplete 4-byte code point for '😁' at the end of the stream
+  stringstream stream(input, std::ios_base::in);
+
+  FlatText text = FlatText::build(stream, input.size(), "UTF8", 5, [&](size_t percent_done) {});
+  REQUIRE(text == FlatText { u"ab" "\ufffd" "\ufffd" });
 }
 
 TEST_CASE("handles characters that require two 16-bit code units") {
