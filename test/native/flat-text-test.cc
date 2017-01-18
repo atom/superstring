3bd53c73a0e9443ae72b170ff62645813c61d464
Max Brunsfeld <maxbrunsfeld@gmail.com>
2017-01-18 13:49:30 -0700

    Handle Unicode characters requiring 2 16-bit code points
    Signed-off-by: Nathan Sobo <nathan@github.com>

--- test/native/flat-text-test.cc
+++ test/native/flat-text-test.cc
@@ -27,6 +27,14 @@ TEST_CASE("replaces invalid byte sequences with the Unicode replacement characte
     progress_reports.push_back(percent_done);
   });
 
-  REQUIRE(text == FlatText { u"ab" "\uFFFD" "\uFFFD" "de" });
+  REQUIRE(text == FlatText { u"ab" "\ufffd" "\ufffd" "de" });
   REQUIRE(progress_reports == vector<size_t>({ 3, 6 }));
+}
+
+TEST_CASE("handles characters that require two 16-bit code units") {
+  string input = "ab" "\xf0\x9f\x98\x81" "cd"; // 'ab😁cd'
+  stringstream stream(input, std::ios_base::in);
+
+  FlatText text = FlatText::build(stream, input.size(), "UTF8", 5, [&](size_t percent_done) {});
+  REQUIRE(text == FlatText { u"ab" "\xd83d" "\xde01" "cd" });
 }
