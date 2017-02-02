f950f3d7c1a0da8da6351c33954ea583bbbbb2df
Nathan Sobo <nathan@github.com>
2017-02-02 13:49:02 -0700

    Fix native patch tests
    Signed-off-by: Max Brunsfeld <maxbrunsfeld@gmail.com>

--- test/native/patch-test.cc
+++ test/native/patch-test.cc
@@ -10,58 +10,67 @@ TEST_CASE("Records simple non-overlapping splices") {
   patch.splice(Point{0, 5}, Point{0, 3}, Point{0, 4});
   patch.splice(Point{0, 10}, Point{0, 3}, Point{0, 4});
   REQUIRE(patch.get_changes() == vector<Change>({
-    Change{
+    Change {
       Point{0, 5}, Point{0, 8},
       Point{0, 5}, Point{0, 9},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     },
-    Change{
+    Change {
       Point{0, 9}, Point{0, 12},
       Point{0, 10}, Point{0, 14},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     }
   }));
 
   patch.splice(Point{0, 2}, Point{0, 2}, Point{0, 1});
   REQUIRE(patch.get_changes() == vector<Change>({
-    Change{
+    Change {
       Point{0, 2}, Point{0, 4},
       Point{0, 2}, Point{0, 3},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     },
-    Change{
+    Change {
       Point{0, 5}, Point{0, 8},
       Point{0, 4}, Point{0, 8},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     },
-    Change{
+    Change {
       Point{0, 9}, Point{0, 12},
       Point{0, 9}, Point{0, 13},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     }
   }));
 
   patch.splice(Point{0, 0}, Point{0, 0}, Point{0, 10});
   REQUIRE(patch.get_changes() == vector<Change>({
-    Change{
+    Change {
       Point{0, 0}, Point{0, 0},
       Point{0, 0}, Point{0, 10},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     },
-    Change{
+    Change {
       Point{0, 2}, Point{0, 4},
       Point{0, 12}, Point{0, 13},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     },
-    Change{
+    Change {
       Point{0, 5}, Point{0, 8},
       Point{0, 14}, Point{0, 18},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     },
-    Change{
+    Change {
       Point{0, 9}, Point{0, 12},
       Point{0, 19}, Point{0, 23},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     }
   }));
 }
@@ -77,11 +86,12 @@ TEST_CASE("Records overlapping splices with text") {
     Text {u"1234"}
   );
   REQUIRE(patch.get_changes() == vector<Change>({
-    Change{
+    Change {
       Point{0, 5}, Point{0, 8},
       Point{0, 5}, Point{0, 9},
       get_text(u"abc").get(),
-      get_text(u"1234").get()
+      get_text(u"1234").get(),
+      0, 0
     },
   }));
 
@@ -94,11 +104,12 @@ TEST_CASE("Records overlapping splices with text") {
     Text {u"5678"}
   );
   REQUIRE(patch.get_changes() == vector<Change>({
-    Change{
+    Change {
       Point{0, 5}, Point{0, 9},
       Point{0, 5}, Point{0, 11},
       get_text(u"abcd").get(),
-      get_text(u"125678").get()
+      get_text(u"125678").get(),
+      0, 0
     },
   }));
 
@@ -115,7 +126,8 @@ TEST_CASE("Records overlapping splices with text") {
       Point{0, 3}, Point{0, 9},
       Point{0, 3}, Point{0, 12},
       get_text(u"efabcd").get(),
-      get_text(u"123425678").get()
+      get_text(u"123425678").get(),
+      0, 0
     },
   }));
 
@@ -128,17 +140,19 @@ TEST_CASE("Records overlapping splices with text") {
     Text {u"5678"}
   );
   REQUIRE(patch.get_changes() == vector<Change>({
-    Change{
+    Change {
       Point{0, 3}, Point{0, 9},
       Point{0, 3}, Point{0, 12},
       get_text(u"efabcd").get(),
-      get_text(u"123425678").get()
+      get_text(u"123425678").get(),
+      0, 0
     },
-    Change{
+    Change {
       Point{0, 12}, Point{0, 15},
       Point{0, 15}, Point{0, 19},
       get_text(u"ghi").get(),
-      get_text(u"5678").get()
+      get_text(u"5678").get(),
+      6, 9
     },
   }));
 
@@ -151,11 +165,12 @@ TEST_CASE("Records overlapping splices with text") {
     Text {u"99999"}
   );
   REQUIRE(patch.get_changes() == vector<Change>({
-    Change{
+    Change {
       Point{0, 1}, Point{0, 18},
       Point{0, 1}, Point{0, 6},
       get_text(u"xxefabcdyyyghizzz").get(),
-      get_text(u"99999").get()
+      get_text(u"99999").get(),
+      0, 0
     }
   }));
 }
@@ -169,25 +184,29 @@ TEST_CASE("Serializes and deserializes") {
   patch.splice(Point{0, 0}, Point{0, 0}, Point{0, 10});
   patch.change_for_old_position(Point{0, 5}); // splay the middle
   REQUIRE(patch.get_changes() == vector<Change>({
-    Change{
+    Change {
       Point{0, 0}, Point{0, 0},
       Point{0, 0}, Point{0, 10},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     },
-    Change{
+    Change {
       Point{0, 2}, Point{0, 4},
       Point{0, 12}, Point{0, 13},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     },
-    Change{
+    Change {
       Point{0, 5}, Point{0, 8},
       Point{0, 14}, Point{0, 18},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     },
-    Change{
+    Change {
       Point{0, 9}, Point{0, 12},
       Point{0, 19}, Point{0, 23},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     }
   }));
 
@@ -195,25 +214,29 @@ TEST_CASE("Serializes and deserializes") {
   patch.serialize(serializer);
   Patch patch_copy(serializer);
   REQUIRE(patch_copy.get_changes() == vector<Change>({
-    Change{
+    Change {
       Point{0, 0}, Point{0, 0},
       Point{0, 0}, Point{0, 10},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     },
-    Change{
+    Change {
       Point{0, 2}, Point{0, 4},
       Point{0, 12}, Point{0, 13},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     },
-    Change{
+    Change {
       Point{0, 5}, Point{0, 8},
       Point{0, 14}, Point{0, 18},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     },
-    Change{
+    Change {
       Point{0, 9}, Point{0, 12},
       Point{0, 19}, Point{0, 23},
-      nullptr, nullptr
+      nullptr, nullptr,
+      0, 0
     }
   }));
 
