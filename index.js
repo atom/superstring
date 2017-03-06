1aa3cee563144387890589daf3c401daf74345f2
Max Brunsfeld <maxbrunsfeld@gmail.com>
2017-03-06 15:16:00 -0800

    Change environment variable used to force browser version

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-if (process.env.FORCE_BROWSER_FALLBACK) {
-    module.exports = require('./browser');
+if (process.env.SUPERSTRING_USE_BROWSER_VERSION) {
+  module.exports = require('./browser');
 } else {
-    try {
-        module.exports = require('./build/Release/superstring.node')
-    } catch (e) {
-        module.exports = require('./build/Debug/superstring.node')
-    }
+  try {
+    module.exports = require('./build/Release/superstring.node')
+  } catch (e) {
+    module.exports = require('./build/Debug/superstring.node')
+  }
 }
