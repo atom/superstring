41881de2c40bbe19fbf5bd3c387b1fe65fbc1618
Max Brunsfeld <maxbrunsfeld@gmail.com>
2017-03-08 17:11:17 -0800

    Add flags for running valgrind and dot to test-native script

--- script/test-native.js
+++ script/test-native.js
@@ -5,6 +5,8 @@ const path = require('path')
 const {spawnSync} = require('child_process')
 
 const testsPath = path.resolve(__dirname, '..', 'build', 'Debug', 'tests')
+const dotPath = path.resolve(__dirname, '..', 'build', 'debug.dot')
+const htmlPath = path.join(__dirname, '..', 'build', 'debug.html')
 
 if (fs.existsSync(testsPath)) {
   run('node-gyp', ['build'])
@@ -14,14 +16,42 @@ if (fs.existsSync(testsPath)) {
 
 const args = process.argv.slice(2)
 
-if (args[0] == '-d') {
-  args.shift()
-  run('lldb', [testsPath, ...args])
-} else {
-  run(testsPath, args)
+switch (args[0]) {
+  case '-d':
+  case '--debug':
+    args.shift()
+    run('lldb', [testsPath, '--', ...args])
+    break
+
+  case '-v':
+  case '--valgrind':
+    args.shift()
+    run('valgrind', [testsPath, '--', ...args])
+    break
+
+  case '-s':
+  case '--svg':
+    args.shift()
+
+    let dotFile = fs.openSync(dotPath, 'w')
+    const {status} = spawnSync(testsPath, args, {stdio: ['ignore', 1, dotFile]})
+    fs.closeSync(dotFile)
+
+    dotFile = fs.openSync(dotPath, 'r')
+    let htmlFile = fs.openSync(htmlPath, 'w')
+    fs.writeSync(htmlFile, '<!doctype HTML>\n<style>svg {width: 100%;}</style>\n')
+    spawnSync('dot', ['-Tsvg'], {stdio: [dotFile, htmlFile, 2]})
+    spawnSync('open', [htmlPath])
+
+    process.exit(status)
+    break
+
+  default:
+    run(testsPath, args)
+    break
 }
 
-function run(command, args = []) {
-  const {status} = spawnSync(command, args, {stdio: 'inherit'})
+function run(command, args = [], options = {stdio: 'inherit'}) {
+  const {status} = spawnSync(command, args, options)
   if (status !== 0) process.exit(status)
 }
