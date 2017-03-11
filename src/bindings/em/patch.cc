79e285caef5948f1ba7b951caea9f924dc49663d
Max Brunsfeld <maxbrunsfeld@gmail.com>
2017-03-10 16:35:09 -0800

    Get emscripten patch binding compiling again

--- src/bindings/em/patch.cc
+++ src/bindings/em/patch.cc
@@ -5,6 +5,8 @@
 #include <emscripten/bind.h>
 #include <emscripten/val.h>
 
+using std::runtime_error;
+using std::string;
 using std::vector;
 
 template <>
@@ -24,11 +26,11 @@ inline Patch const *emscripten::val::as<Patch const *>(void) const {
 }
 
 Patch *constructor(emscripten::val value) {
-  bool merge_adjacent_hunks = false;
-  if (value.as<bool>() && value["mergeAdjacentHunks"].as<bool>()) {
-    merge_adjacent_hunks = true;
+  bool merge_adjacent_changes = false;
+  if (value.as<bool>() && value["mergeAdjacentChanges"].as<bool>()) {
+    merge_adjacent_changes = true;
   }
-  return new Patch(merge_adjacent_hunks);
+  return new Patch(merge_adjacent_changes);
 }
 
 vector<uint8_t> serialize(Patch const &patch) {
@@ -47,35 +49,55 @@ Patch *deserialize(const vector<uint8_t> &bytes) {
   return new Patch(deserializer);
 }
 
+void splice(Patch &patch, PointWrapper start, PointWrapper deleted_extent, PointWrapper inserted_extent) {
+  bool result = patch.splice(
+    start,
+    deleted_extent,
+    inserted_extent
+  );
+  if (!result) throw runtime_error("Can't splice into a frozen patch");
+}
+
+void splice_with_text(Patch &patch, PointWrapper start, PointWrapper deleted_extent, PointWrapper inserted_extent,
+                      const string &deleted_text, const string &inserted_text) {
+  bool result = patch.splice(
+    start,
+    deleted_extent,
+    inserted_extent,
+    Text(deleted_text.begin(), deleted_text.end()),
+    Text(inserted_text.begin(), inserted_text.end())
+  );
+  if (!result) throw runtime_error("Can't splice into a frozen patch");
+}
+
 template <typename T>
-void hunk_set_noop(Patch::Hunk & hunk, T const &) {}
+void change_set_noop(Patch::Change &change, T const &) {}
 
 EMSCRIPTEN_BINDINGS(Patch) {
   emscripten::class_<Patch>("Patch")
     .constructor<>()
     .constructor<emscripten::val>(WRAP_STATIC(&constructor), emscripten::allow_raw_pointers())
-    .function("splice", WRAP_OVERLOAD(&Patch::splice, bool (Patch::*)(Point, Point, Point)))
-    .function("splice", WRAP_OVERLOAD(&Patch::splice, bool (Patch::*)(Point, Point, Point, std::unique_ptr<Text>, std::unique_ptr<Text>)))
+    .function("splice", splice)
+    .function("splice", splice_with_text)
     .function("spliceOld", WRAP(&Patch::splice_old))
     .function("copy", WRAP(&Patch::copy))
     .function("invert", WRAP(&Patch::invert))
-    .function("getHunks", WRAP(&Patch::get_hunks))
-    .function("getHunksInNewRange", WRAP_OVERLOAD(&Patch::get_hunks_in_new_range, std::vector<Patch::Hunk> (Patch::*)(Point, Point)))
-    .function("getHunksInNewRange", WRAP_OVERLOAD(&Patch::get_hunks_in_new_range, std::vector<Patch::Hunk> (Patch::*)(Point, Point, bool)))
-    .function("getHunksInOldRange", WRAP(&Patch::get_hunks_in_old_range))
-    .function("getHunkCount", WRAP(&Patch::get_hunk_count))
-    .function("hunkForOldPosition", WRAP(&Patch::hunk_for_old_position))
-    .function("hunkForNewPosition", WRAP(&Patch::hunk_for_new_position))
+    .function("getChanges", WRAP(&Patch::get_changes))
+    .function("getChangesInNewRange", WRAP_OVERLOAD(&Patch::get_changes_in_new_range, std::vector<Patch::Change> (Patch::*)(Point, Point, bool)))
+    .function("getChangesInOldRange", WRAP(&Patch::get_changes_in_old_range))
+    .function("getChangeCount", WRAP(&Patch::get_change_count))
+    .function("changeForOldPosition", WRAP(&Patch::change_for_old_position))
+    .function("changeForNewPosition", WRAP(&Patch::change_for_new_position))
     .function("rebalance", WRAP(&Patch::rebalance))
     .function("serialize", WRAP(&serialize))
     .class_function("compose", WRAP_STATIC(&compose), emscripten::allow_raw_pointers())
     .class_function("deserialize", WRAP_STATIC(&deserialize), emscripten::allow_raw_pointers());
 
-  emscripten::value_object<Patch::Hunk>("Hunk")
-    .field("oldStart", WRAP_FIELD(Patch::Hunk, old_start))
-    .field("oldEnd", WRAP_FIELD(Patch::Hunk, old_end))
-    .field("newStart", WRAP_FIELD(Patch::Hunk, new_start))
-    .field("newEnd", WRAP_FIELD(Patch::Hunk, new_end))
-    .field("oldText", WRAP_FIELD(Patch::Hunk, old_text))
-    .field("newText", WRAP_FIELD(Patch::Hunk, new_text));
+  emscripten::value_object<Patch::Change>("Change")
+    .field("oldStart", WRAP_FIELD(Patch::Change, old_start))
+    .field("oldEnd", WRAP_FIELD(Patch::Change, old_end))
+    .field("newStart", WRAP_FIELD(Patch::Change, new_start))
+    .field("newEnd", WRAP_FIELD(Patch::Change, new_end))
+    .field("oldText", WRAP_FIELD(Patch::Change, old_text))
+    .field("newText", WRAP_FIELD(Patch::Change, new_text));
 }
