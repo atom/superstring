e5c654f1a0ae065ca875946037d0cae4e176238f
Max Brunsfeld <maxbrunsfeld@gmail.com>
2017-04-11 22:58:13 -0700

    Add a text_diff test for a changes on a single line

--- test/native/text-diff-test.cc
+++ test/native/text-diff-test.cc
@@ -5,7 +5,7 @@
 using Change = Patch::Change;
 using std::vector;
 
-TEST_CASE("text_diff - basic") {
+TEST_CASE("text_diff - multiple lines") {
   Text old_text{u"abc\nghi\njk\nmno\n"};
   Text new_text{u"abc\ndef\nghi\njkl\nmno\n"};
 
@@ -22,5 +22,25 @@ TEST_CASE("text_diff - basic") {
       Point{3, 2}, Point{3, 3},
       nullptr, nullptr, 0, 0
     }
+  }));
+}
+
+TEST_CASE("text_diff - single line") {
+  Text old_text{u"abcdefghij"};
+  Text new_text{u"abcxyefij"};
+
+  Patch patch = text_diff(old_text, new_text);
+
+  REQUIRE(patch.get_changes() == vector<Change>({
+    Change{
+      Point{0, 3}, Point{0, 4},
+      Point{0, 3}, Point{0, 5},
+      nullptr, nullptr, 0, 0
+    },
+    Change{
+      Point{0, 6}, Point{0, 8},
+      Point{0, 7}, Point{0, 7},
+      nullptr, nullptr, 0, 0
+    },
   }));
 }
