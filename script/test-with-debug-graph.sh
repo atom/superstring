9807f099490357d0b7595e4490fb57027b3f3ce0
Max Brunsfeld <maxbrunsfeld@gmail.com>
2017-05-11 11:18:45 -0700

    In test-with-debug-graph, put generated files in build folder

--- script/test-with-debug-graph.sh
+++ script/test-with-debug-graph.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo '<?xml version="1.0"?>' > debug.html
-echo '<style> svg { width: 100%; margin-bottom: 10px; } </style>' >> debug.html
-node_modules/.bin/mocha test/js/*.js 2> >(dot -Tsvg >> debug.html)
+echo '<?xml version="1.0"?>' > build/debug.html
+echo '<style> svg { width: 100%; margin-bottom: 10px; } </style>' >> build/debug.html
+node_modules/.bin/mocha test/js/*.js 2> >(dot -Tsvg >> build/debug.html)
+open build/debug.html
